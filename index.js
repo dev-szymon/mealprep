ff3f1fa50fe63ac601ce610f1ab1c8a0e5fd54b2
Szymon <szymonphotoshop@gmail.com>
2020-06-03 15:15:22 +0200

    apollo https config

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 const typeDefs = require('./schema/typeDefs/index');
 const resolvers = require('./schema/resolvers/index');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
 
-const server = new ApolloServer({
+const configurations = {
+  // Note: You may need sudo to run on port 443
+  production: { ssl: true, port: 443, hostname: 'eatwell.club' },
+  development: { ssl: false, port: 4000, hostname: 'localhost' },
+};
+
+const environment = process.env.NODE_ENV;
+const config = configurations[environment];
+
+const apollo = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req, res }) => {
@@ -18,11 +30,33 @@ const server = new ApolloServer({
 
 const app = express();
 
-server.applyMiddleware({ app });
+apollo.applyMiddleware({ app });
 connectDB();
+
+var server;
+if (config.ssl) {
+  // Assumes certificates are in a .ssl folder off of the package root. Make sure
+  // these files are secured.
+  server = https.createServer(
+    {
+      key: fs.readFileSync(`./ssl/${environment}/server.key`),
+      cert: fs.readFileSync(`./ssl/${environment}/server.crt`),
+    },
+    app
+  );
+} else {
+  server = http.createServer(app);
+}
 
 app.get('/', (req, res) => {
   res.send('<h1>Mealprep API</h1>');
 });
 
-app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+server.listen(PORT, () =>
+  console.log(
+    '🚀 Server ready at',
+    `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${
+      apollo.graphqlPath
+    }`
+  )
+);
