e6199fe3eef41e0de706247497bba92fc9866a67
Szymon <szymonphotoshop@gmail.com>
2020-06-04 11:47:12 +0200

    ssl config

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 const typeDefs = require('./schema/typeDefs/index');
 const resolvers = require('./schema/resolvers/index');
-const fs = require('fs');
-const https = require('https');
-const http = require('http');
+// const fs = require('fs');
+// const https = require('https');
+// const http = require('http');
 
-const configurations = {
-  // Note: You may need sudo to run on port 443
-  production: { ssl: true, port: 443, hostname: 'eatwell.club' },
-  development: { ssl: false, port: 4000, hostname: 'localhost' },
-};
+// const configurations = {
+//   // Note: You may need sudo to run on port 443
+//   production: { ssl: true, port: 443, hostname: 'eatwell.club' },
+//   development: { ssl: false, port: 4000, hostname: 'localhost' },
+// };
 
-const environment = process.env.NODE_ENV;
-const config = configurations[environment];
+// const environment = process.env.NODE_ENV;
+// const config = configurations[environment];
 
 const apollo = new ApolloServer({
   typeDefs,
@@ -33,30 +33,31 @@ const app = express();
 apollo.applyMiddleware({ app });
 connectDB();
 
-var server;
-if (config.ssl) {
-  // Assumes certificates are in a .ssl folder off of the package root. Make sure
-  // these files are secured.
-  server = https.createServer(
-    {
-      key: fs.readFileSync(`./ssl/${environment}/server.key`),
-      cert: fs.readFileSync(`./ssl/${environment}/server.crt`),
-    },
-    app
-  );
-} else {
-  server = http.createServer(app);
-}
+// var server;
+// if (config.ssl) {
+//   // Assumes certificates are in a .ssl folder off of the package root. Make sure
+//   // these files are secured.
+//   server = https.createServer(
+//     {
+//       key: fs.readFileSync(`./ssl/${environment}/server.key`),
+//       cert: fs.readFileSync(`./ssl/${environment}/server.crt`),
+//     },
+//     app
+//   );
+// } else {
+//   server = http.createServer(app);
+// }
 
 app.get('/', (req, res) => {
   res.send('<h1>Mealprep API</h1>');
 });
 
-server.listen(PORT, () =>
+app.listen(PORT, () =>
   console.log(
-    '🚀 Server ready at',
-    `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${
-      apollo.graphqlPath
-    }`
+    // '🚀 Server ready at',
+    // `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${
+    //   apollo.graphqlPath
+    // }`
+    `server ready at ${PORT}`
   )
 );
